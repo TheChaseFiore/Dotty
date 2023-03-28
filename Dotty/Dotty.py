6614539a056e43ef28a8df162e459153a6d168a0
Chase Fiore <chasefiore@gmail.com>
2023-03-27 21:03:58 -0400

    Add project files.

---
+++ Dotty/Dotty.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+__author__ = "Chase Fiore"
+__copyright__ = "Copyright 2023, Chase Fiore"
+__license__ = "GPL"
+__version__ = "0.0"
+__email__ = "chasefiore@gmail.com"
+__status__ = "Production"
+
+
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
