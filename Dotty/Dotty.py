56cc5650d87770d810b83e486812587c1311ccbf
TheChaseFiore <96696725+TheChaseFiore@users.noreply.github.com>
2023-03-27 22:51:14 -0400

    Test!

--- Dotty/Dotty.py
+++ Dotty/Dotty.py
@@ -6,7 +6,13 @@ __version__ = "0.0"
 __email__ = "chasefiore@gmail.com"
 __status__ = "Production"
 
+import serial
 
+
+ser = serial.Serial('/dev/ttyUSB0')  # open serial port
+print(ser.name)         # check which port was really used
+ser.write(b'hello')     # write a string
+ser.close()             # close port
 
 def main():
     print("Hello World!")
